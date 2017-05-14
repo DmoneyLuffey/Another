2e29eed9a2c57f7e1a1f89545e2ee10b2c266f94
DemoneyLuffey <DmoneyLuffey@gmail.com>
2017-05-14 13:44:23 -0700

    Working On NPC

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject interaction;
+    public GameObject quest;
+    public Transform toonTrans;
+
+    private float dist;
+    public float maxDist = 2;
+
+    public bool isNear = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (toonTrans != null)
+        {
+            dist = Vector3.Distance(toonTrans.position, transform.position);
+            LookAt();
+            PlayerSearch();
+        }
+    }
+
+    private void LookAt()
+    {
+        if (dist <= maxDist)
+        {
+            Quaternion rotation = Quaternion.LookRotation(toonTrans.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        }
+    }
+
+    private void PlayerSearch()
+    {
+        Vector3 dirToToon = (toonTrans.transform.position - this.transform.position).normalized;
+        Ray ray = new Ray(this.transform.position, dirToToon);
+        if (dist <= maxDist)
+        {
+            isNear = true;
+        }
+        else if (dist > maxDist)
+        {
+            isNear = false;
+        }
+    }
+}
