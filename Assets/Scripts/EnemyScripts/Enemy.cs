2d5142fc184c6c9736edaf17fb28cb99ae08ac96
Unknown <DmoneyLuffey@gmail.com>
2017-05-22 20:56:31 -0700

    Enemies Respawn On Death/ PickUps Work

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Transform enemyRespawn;
+    public Transform toonTrans;
+    private Rigidbody rb;
+
+    public float currentHealth;
+    private float health = 10;
+    private float dist;
+    public float maxDist = 2;
+    public float distCheck = 10.0f;
+    public float moveSpeed = 5.0f;
+    private float damageStart = 0f;
+    public float damageCoolDown = 2.0f;
+    public float playerDamage = 1.0f;
+
+    private float respawnStart = 0;
+    public float respawnCoolDown = 3;
+    public bool waitingForRespawn = false;
+
+    public bool canAttack = false;
+    public bool isAttacking = true;
+    private bool isNear = false;
+    public bool isDead = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        isDead = false;
+        currentHealth = health;
+        rb = this.GetComponent<Rigidbody>();
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        toonTrans = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        //enemyRespawn = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toonTrans != null)
+        {
+            dist = Vector3.Distance(toonTrans.position, transform.position);
+            PlayerSearch();
+            LookAt();
+            EnemyDeath();
+        }
+    }
+    private void LookAt()
+    {
+        if(dist <= maxDist)
+        {
+            Quaternion rotation = Quaternion.LookRotation(toonTrans.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        }
+    }
+    private void PlayerSearch()
+    {
+        Vector3 dirToToon = (toonTrans.transform.position - this.transform.position).normalized;
+        Ray ray = new Ray(this.transform.position, dirToToon);
+        if (dist < maxDist)
+        {
+            isNear = true;
+            Vector3 movementDir = this.transform.forward;
+            movementDir.Normalize();
+            transform.LookAt(toonTrans);
+            this.rb.MovePosition(this.transform.position + movementDir * (moveSpeed * Time.deltaTime));
+        }
+        else
+        {
+            isNear = false;
+        }
+        if (dist <= 1.5 && !isDead)
+        {
+            canAttack = true;
+            if (canAttack)
+            {
+                isAttacking = true;
+                //Debug.Log("Hey, I'm hitting you.");
+                DamagePlayer(playerDamage);
+            }
+        }
+        if (dist > 1.5 && isNear)
+        {
+            canAttack = false;
+            if(!canAttack)
+            {
+                isAttacking = false;
+                if(!isAttacking)
+                {
+                    playerHealth.isHit = false;
+                }
+            }
+        }
+    }
+
+    public void DamagePlayer(float playerDamage)
+    {
+        if(canAttack && isAttacking)
+        {
+            if (Time.time > damageStart + damageCoolDown)
+            {
+                if (isAttacking)
+                {
+                    playerHealth.isHit = true;
+                    damageStart = Time.time;
+                    playerHealth.Damage(playerDamage);
+                    //anim.SetTrigger("Attack");
+                }
+            }
+        }
+    }
+    public void EnemyDeath()
+    {
+        if(currentHealth <= 0)
+        {
+            transform.position = transform.position + Vector3.up * 1000; //just need to put him some place off screen
+            waitingForRespawn = true;
+        }
+
+        if (waitingForRespawn)
+        {
+            respawnStart += Time.deltaTime;
+            if (respawnStart >= respawnCoolDown)
+            {
+                Respawn();
+                waitingForRespawn = false;
+                currentHealth = health;
+                respawnStart = 0;
+            }
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = enemyRespawn.position;
+        transform.rotation = enemyRespawn.rotation;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    Rect rect;
+
+    public string levelToLoad = "";
+
+    public float currentHealth;
+    private float health = 10;
+    private Texture heartTexture;
+    private Texture emptyTexture;
+    private float healthStart = 0f;
+    public float healthCoolDown = 2f;
+    private float damageStart = 0f;
+    public float damageCoolDown = 2f;
+    public float healthDepletion = 1;
+
+    public bool isAttacked = false;
+    public bool isHealing = false;
+    public bool isHit = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentHealth = health;
+
+        rect = new Rect(Screen.width * 0.015f, Screen.height * 0.15f, Screen.width * 0.015f, Screen.width * 0.05f); //Places Texture in Proper position
+        heartTexture = Resources.Load("Images/Heart") as Texture; //Loads texture being used
+        emptyTexture = Resources.Load("Images/EmptyHeart") as Texture;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Damage(0);
+    }
+
+    void OnGUI()
+    {
+        if(isAttacked)
+        {
+            for (int i = 0; i < currentHealth; i++)
+            {
+                Rect newRect = new Rect(rect.x, rect.y, rect.width, rect.width); //Positions array of textures
+
+                GUI.DrawTexture(new Rect(rect.x * (3f * i + 1), rect.y - 100, 70, 60), heartTexture); //Draws textrues
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Rect newRect = new Rect(rect.x, rect.y, rect.width, rect.width); //Positions array of textures
+
+                GUI.DrawTexture(new Rect(rect.x * (3f * i + 1), rect.y - 100, 70, 60), emptyTexture); //Draws textrues
+            }
+        }
+
+        if (isHealing)
+        {
+            for (int i = 0; i < currentHealth; i++)
+            {
+                Rect newRect = new Rect(rect.x, rect.y, rect.width, rect.width); //Positions array of textures
+
+                GUI.DrawTexture(new Rect(rect.x * (3f * i + 1), rect.y - 100, 70, 60), heartTexture); //Draws textrues
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Rect newRect = new Rect(rect.x, rect.y, rect.width, rect.width); //Positions array of textures
+
+                GUI.DrawTexture(new Rect(rect.x * (3f * i + 1), rect.y - 100, 70, 60), emptyTexture); //Draws textrues
+            }
+        }
+
+    }
+
+    public void Damage(float amount)
+    {
+        if(isHit)
+        {
+            currentHealth -= amount;
+            isAttacked = true;
+        }
+
+        if (isAttacked && !isHit)
+        {
+            damageStart += Time.deltaTime;
+            if (damageStart < damageCoolDown + Time.deltaTime)
+            {
+                if(damageStart >= damageCoolDown && !isHit)
+                {
+                    damageStart = 0;
+                    isAttacked = false;
+                }
+            }
+        }
+    }
+    public void Heal(float amount)
+    {
+        if (isHealing)
+        {
+            currentHealth += amount;
+            isAttacked = true;
+        }
+
+        if (isHealing)
+        {
+            healthStart += Time.deltaTime;
+            if (healthStart < healthCoolDown + Time.deltaTime)
+            {
+                if (healthStart >= healthCoolDown && !isHealing)
+                {
+                    healthStart = 0;
+                    isAttacked = false;
+                }
+            }
+        }
+    }
+}
