167140790795ac629bed1913ea757516d2d370a8
Unknown <DmoneyLuffey@gmail.com>
2017-05-24 21:01:44 -0700

    Working On Enemy Drops

--- Assets/Scripts/ResourceScripts/Collect.cs
+++ Assets/Scripts/ResourceScripts/Collect.cs
@@ -5,23 +5,32 @@ using System.Collections;
 public class Collect : MonoBehaviour
 {
     public GameObject player;
-
+    public Transform target;
     public ResourseUI playerUI;
+    public Transform thisTransform;
 
     public int vialAmount;
+    public float rotationSpeed;
+    public float moveSpeed;
 
     // Use this for initialization
     void Start ()
     {
+        moveSpeed = Random.Range(1, 7);
         playerUI = GameObject.FindGameObjectWithTag("Player").GetComponent<ResourseUI>();
         player = GameObject.FindGameObjectWithTag("Player");
+        target = GameObject.FindWithTag("Player").transform;
+        thisTransform = transform;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        thisTransform.rotation = Quaternion.Slerp(thisTransform.rotation,
+        Quaternion.LookRotation(target.position - thisTransform.position), rotationSpeed * Time.deltaTime);
+        thisTransform.position += thisTransform.forward * moveSpeed * Time.deltaTime;
+    }
+    
 
     void OnTriggerEnter(Collider other)
     {
@@ -31,6 +40,7 @@ public class Collect : MonoBehaviour
             playerUI.vials = playerUI.vials + vialAmount;
             playerUI.vialsText.text = "Vials: " + playerUI.vials.ToString();
             print("Hello");
+            Destroy(this.gameObject);
         }
     }
 }
