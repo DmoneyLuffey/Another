d51bf2e1c3c584c9240a5cadf3e8c023f1dce817
DemoneyLuffey <DmoneyLuffey@gmail.com>
2017-05-25 01:20:10 -0700

    Thing

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour
+{
+
+    AudioSource[] asc;
+    //private static float vol = GameObject.GetComponents<AudioSource>().volume;
+    public AudioSource MasterAudioSource;
+    // Use this for initialization
+    void Start()
+    {
+        asc = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetVolume();
+    }
+
+    public void SetVolume()
+    {
+        for (int i = 0; i < asc.Length; i++)
+        {
+            asc[i].volume = MasterAudioSource.volume;
+        }
+    }
+}
