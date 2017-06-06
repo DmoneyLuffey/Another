c9b23adc55d213148a613e3450ace57377d78f06
Unknown <ravenpilotz0@gmail.com>
2017-06-05 19:28:51 -0700

    Block puzzle
    pushed again

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    public GameObject rock;
+    public GameObject Wall;
+    public bool pressed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            Destroy(Wall);
+        }
+    }
+
+    void OnTriggerEnter(Collider rock)
+    {
+        pressed = true;
+    }
+}
